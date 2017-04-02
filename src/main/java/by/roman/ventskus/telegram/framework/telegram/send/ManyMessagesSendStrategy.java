a89925ada178bc2ed172a6152c9c41b66e8fa439
Roman Ventskus <roman.ventskus@gmail.com>
2017-04-02 22:18:36 +0300

    Fix for ManyMessagesResponse - menu only for last message

--- src/main/java/by/roman/ventskus/telegram/framework/telegram/send/ManyMessagesSendStrategy.java
+++ src/main/java/by/roman/ventskus/telegram/framework/telegram/send/ManyMessagesSendStrategy.java
@@ -3,6 +3,8 @@ package by.roman.ventskus.telegram.framework.telegram.send;
 import by.roman.ventskus.telegram.framework.entity.response.ManyMessagesResponse;
 import by.roman.ventskus.telegram.framework.telegram.TelegramAPI;
 
+import java.util.List;
+
 /**
  * Created by Roman Ventskus on 24.04.2016.
  */
@@ -14,6 +16,14 @@ public class ManyMessagesSendStrategy extends SendStrategy<ManyMessagesResponse>
 
     @Override
     void send(ManyMessagesResponse response) {
-        response.getMessages().forEach(s -> getTelegramAPI().send(s, response.getUser(), response.getReplyKeyboardMarkup()));
+        List<String> messages = response.getMessages();
+        if (messages.size() > 1) {
+            messages.stream()
+                    .limit(Math.max(0, messages.size() - 1))
+                    .forEach(s -> getTelegramAPI().send(s, response.getUser(), null));
+        }
+        messages.stream()
+                .skip(Math.max(0, messages.size() - 1))
+                .forEach(s -> getTelegramAPI().send(s, response.getUser(), response.getReplyKeyboardMarkup()));
     }
 }
